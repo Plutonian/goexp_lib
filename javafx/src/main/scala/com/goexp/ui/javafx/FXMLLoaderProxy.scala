b60aab744bf37f1ab57e1359e444474945fca046
benbear <benbear0519@gmail.com>
2020-05-16 08:03:57 +0800

    upgrade java to 13.0.2

--- javafx/src/main/scala/com/goexp/ui/javafx/FXMLLoaderProxy.scala
+++ javafx/src/main/scala/com/goexp/ui/javafx/FXMLLoaderProxy.scala
@@ -7,11 +7,11 @@ import javafx.fxml.FXMLLoader
 import scala.reflect.ClassTag
 
 class FXMLLoaderProxy[N, C](val url: URL) {
+  final private val loader = new FXMLLoader(url);
   final val node: N = loader.load
   final val controller: C = loader.getController
-  final private val loader = new FXMLLoader(url);
 
-  def this(path: String)(implicit target: ClassTag[C]) {
+  def this(path: String)(implicit target: ClassTag[C]) = {
     this(target.runtimeClass.getResource(path))
   }
 
